95107f68ae43c46f02bceecff3ca9ec5f6c57efe
jeo <test.gmail.com>
2022-12-02 23:23:34 +0900

    a

--- testBasic.py
+++ testBasic.py
@@ -50,8 +50,8 @@ print("WORKING AS", NAME)
 for i in range(1000):
     steamworks.Users.SendMSG(NAME+b'HELLO'+ bytes(str(i),"utf") )
     steamworks.Users.GetMSG()
-    time.sleep(.1)
-    if i%10==0: steamworks.Users.RunCallbacks()
+    time.sleep(1/60)
+    if i%100==0: steamworks.Users.RunCallbacks()
 
 #print(f'Logged on as {my_steam64}, level: {my_steam_level}')
 #x = steamworks.UserStats.FindLeaderboard('Quickest Win', retLB)
